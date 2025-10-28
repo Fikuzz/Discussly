86c399f944950082a5d4e8b2d6e15cf508ec2b57
Franak Tumash <franak9@yandex.ru>
2025-10-28 23:33:33 +0300

    refactor: add postId in CommentDto

--- Discussly.Core/DTOs/Comment/CommentDto.cs
+++ Discussly.Core/DTOs/Comment/CommentDto.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Text { get; set; } = string.Empty;
+        public Guid PostId { get; set; }
         public UserDto Author { get; set; } = new();
         public DateTime CreatedAt { get; set; }
         public bool IsEditing { get; set; } = false;
